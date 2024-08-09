0d1a684edc7cd840132887c165010f26ec5f28c4
Lamek84 <kkemal84@yahoo.de>
2024-08-09 08:34:22 +0200

    Create Pet model with new Classes User, AnimalType, Credentials(Rekviziti)

---
+++ backend/src/FindAnimal.Domain/AnimalType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAnimal.Domain
+{
+    public class AnimalType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
