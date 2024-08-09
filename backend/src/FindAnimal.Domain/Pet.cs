41c1bc81e91fcbac12397b77f1a02fd60c9a2d22
Lamek84 <kkemal84@yahoo.de>
2024-08-09 11:24:49 +0200

    add private to setter

--- backend/src/FindAnimal.Domain/Pet.cs
+++ backend/src/FindAnimal.Domain/Pet.cs
@@ -2,23 +2,23 @@
 {
     public class Pet
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public AnimalType Type { get; set; }
-        public string Description { get; set; }
-        public string Breed { get; set; }
-        public string Color { get; set; }
-        public string HelthInformation { get; set; }
-        public string LocationAddress { get; set; }
-        public int Height { get; set; }
-        public int Weigth { get; set; }
-        public string ContactNumber {  get; set; }
-        public DateTime BirthDate { get; set; }
-        public bool IsCastrated { get; set; }
-        public bool IsVaccinated { get; set; }
-        public string HelpStatus { get; set; }
-        public List<Credentials> Credentials { get; set; }
-        public DateTime? creationDate { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public AnimalType Type { get; private set; }
+        public string Description { get; private set; }
+        public string Breed { get; private set; }
+        public string Color { get; private set; }
+        public string HelthInformation { get; private set; }
+        public string LocationAddress { get; private set; }
+        public int Height { get; private set; }
+        public int Weigth { get; private set; }
+        public string ContactNumber { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public bool IsCastrated { get; private set; }
+        public bool IsVaccinated { get; private set; }
+        public string HelpStatus { get; private set; }
+        public List<Credentials> Credentials { get; private set; }
+        public DateTime? creationDate { get; private set; }
 
     }
 }
