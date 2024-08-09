a2debdf91b846beefbac5c1a3e72dbfb38dc4787
Lamek84 <kkemal84@yahoo.de>
2024-08-09 21:13:12 +0200

    Change Entites Id to guid, and clean with oders Domin layer

--- backend/src/FindAnimal.Domain/Entities/PetPhoto.cs
+++ backend/src/FindAnimal.Domain/Entities/PetPhoto.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FindAnimal.Domain
+namespace FindAnimal.Domain.Entities
 {
     public class PetPhoto
     {
-        public int Id { get; private set; }
+        public Guid Id { get; private set; }
         public string Title { get; private set; }
         public string Path { get; private set; }
         public bool IsMain { get; private set; }
